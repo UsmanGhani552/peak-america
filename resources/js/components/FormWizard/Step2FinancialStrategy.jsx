301325e6ce23d62aa80e136635bff7e2fa0d0621
UsmanGhani552 <usman.centosquare@gmail.com>
2025-05-22 19:20:07 +0500

    dynamic frontend

--- resources/js/components/FormWizard/Step2FinancialStrategy.jsx
+++ resources/js/components/FormWizard/Step2FinancialStrategy.jsx
@@ -83,19 +83,19 @@ function Step2FinancialStrategy() {
                                             <div className="personal-detail-input">
                                                 <h2>You</h2>
                                                 <label className="form-label responsive-label">Check/ Savings/ Money Market</label>
-                                                <input type="number" onChange={handleYouChange(0)} className="form-control" placeholder="Enter Amount" />
+                                                <input type="number" onChange={handleYouChange(0)} value={youInputArray[0] === 0 ? "" : youInputArray[0]} className="form-control" placeholder="Enter Amount" />
                                                 <label className="form-label responsive-label">CD's</label>
-                                                <input type="number" onChange={handleYouChange(1)} className="form-control" placeholder="Enter Amount" />
+                                                <input type="number" onChange={handleYouChange(1)} value={youInputArray[1] === 0 ? "" : youInputArray[1]} className="form-control" placeholder="Enter Amount" />
                                                 <label className="form-label responsive-label">Stock/ Bonds/ Brokerage</label>
-                                                <input type="number" onChange={handleYouChange(2)} className="form-control" placeholder="Enter Amount" />
+                                                <input type="number" onChange={handleYouChange(2)} value={youInputArray[2] === 0 ? "" : youInputArray[2]} className="form-control" placeholder="Enter Amount" />
                                                 <label className="form-label responsive-label">IRAs (Pre-tax)</label>
-                                                <input type="number" onChange={handleYouChange(3)} className="form-control" placeholder="Enter Amount" />
+                                                <input type="number" onChange={handleYouChange(3)} value={youInputArray[3] === 0 ? "" : youInputArray[3]} className="form-control" placeholder="Enter Amount" />
                                                 <label className="form-label responsive-label">Roth IRAs</label>
-                                                <input type="number" onChange={handleYouChange(4)} className="form-control" placeholder="Enter Amount" />
+                                                <input type="number" onChange={handleYouChange(4)} value={youInputArray[4] === 0 ? "" : youInputArray[4]} className="form-control" placeholder="Enter Amount" />
                                                 <label className="form-label responsive-label">Other Fund/SMA's</label>
-                                                <input type="number" onChange={handleYouChange(5)} className="form-control" placeholder="Enter Amount" />
+                                                <input type="number" onChange={handleYouChange(5)} value={youInputArray[5] === 0 ? "" : youInputArray[5]} className="form-control" placeholder="Enter Amount" />
                                                 <label className="form-label responsive-label">Qualified Retirement Accounts (401k, 403B, TSA)</label>
-                                                <input type="number" onChange={handleYouChange(6)} className="form-control" placeholder="Enter Amount" />
+                                                <input type="number" onChange={handleYouChange(6)} value={youInputArray[6] === 0 ? "" : youInputArray[6]} className="form-control" placeholder="Enter Amount" />
                                                 <label className="form-label responsive-label">Total</label>
                                                 <input type="number" className="form-control" value={youTotal} placeholder="$00.00" />
                                             </div>
@@ -105,19 +105,19 @@ function Step2FinancialStrategy() {
                                             <div className="personal-detail-input">
                                                 <h2>Spouse</h2>
                                                 <label className="form-label responsive-label">Check/ Savings/ Money Market</label>
-                                                <input type="number" onChange={handleSpouseChange(0)} className="form-control" placeholder="Enter Amount" />
+                                                <input type="number" onChange={handleSpouseChange(0)} value={spouseInputArray[0] === 0 ? "" : spouseInputArray[0]} className="form-control" placeholder="Enter Amount" />
                                                 <label className="form-label responsive-label">CD's</label>
-                                                <input type="number" onChange={handleSpouseChange(1)} className="form-control" placeholder="Enter Amount" />
+                                                <input type="number" onChange={handleSpouseChange(1)} value={spouseInputArray[1] === 0 ? "" : spouseInputArray[1]} className="form-control" placeholder="Enter Amount" />
                                                 <label className="form-label responsive-label">Stock/ Bonds/ Brokerage</label>
-                                                <input type="number" onChange={handleSpouseChange(2)} className="form-control" placeholder="Enter Amount" />
+                                                <input type="number" onChange={handleSpouseChange(2)} value={spouseInputArray[2] === 0 ? "" : spouseInputArray[2]} className="form-control" placeholder="Enter Amount" />
                                                 <label className="form-label responsive-label">IRAs (Pre-tax)</label>
-                                                <input type="number" onChange={handleSpouseChange(3)} className="form-control" placeholder="Enter Amount" />
+                                                <input type="number" onChange={handleSpouseChange(3)} value={spouseInputArray[3] === 0 ? "" : spouseInputArray[3]} className="form-control" placeholder="Enter Amount" />
                                                 <label className="form-label responsive-label">Roth IRAs</label>
-                                                <input type="number" onChange={handleSpouseChange(4)} className="form-control" placeholder="Enter Amount" />
+                                                <input type="number" onChange={handleSpouseChange(4)} value={spouseInputArray[4] === 0 ? "" : spouseInputArray[4]} className="form-control" placeholder="Enter Amount" />
                                                 <label className="form-label responsive-label">Other Fund/SMA's</label>
-                                                <input type="number" onChange={handleSpouseChange(5)} className="form-control" placeholder="Enter Amount" />
+                                                <input type="number" onChange={handleSpouseChange(5)} value={spouseInputArray[5] === 0 ? "" : spouseInputArray[5]} className="form-control" placeholder="Enter Amount" />
                                                 <label className="form-label responsive-label">Qualified Retirement Accounts (401k, 403B, TSA)</label>
-                                                <input type="number" onChange={handleSpouseChange(6)} className="form-control" placeholder="Enter Amount" />
+                                                <input type="number" onChange={handleSpouseChange(6)} value={spouseInputArray[6] === 0 ? "" : spouseInputArray[6]} className="form-control" placeholder="Enter Amount" />
                                                 <label className="form-label responsive-label">Total</label>
                                                 <input type="number" className="form-control" value={spouseTotal} placeholder="$00.00" />
                                             </div>
@@ -164,19 +164,19 @@ function Step2FinancialStrategy() {
                                         <div className="personal-detail-input">
                                             <h2>You</h2>
                                             <label className="form-label responsive-label">Home Value</label>
-                                            <input type="number" onChange={handleAssetYouChange(0)} className="form-control" placeholder="Enter Amount" />
+                                            <input type="number" onChange={handleAssetYouChange(0)} value={youAssetArray[0] === 0 ? "" : youAssetArray[0]} className="form-control" placeholder="Enter Amount" />
                                             <label className="form-label responsive-label">Vacation Homes</label>
-                                            <input type="number" onChange={handleAssetYouChange(1)} className="form-control" placeholder="Enter Amount" />
+                                            <input type="number" onChange={handleAssetYouChange(1)} value={youAssetArray[1] === 0 ? "" : youAssetArray[1]} className="form-control" placeholder="Enter Amount" />
                                             <label className="form-label responsive-label">Rental Properties</label>
-                                            <input type="number" onChange={handleAssetYouChange(2)} className="form-control" placeholder="Enter Amount" />
+                                            <input type="number" onChange={handleAssetYouChange(2)} value={youAssetArray[2] === 0 ? "" : youAssetArray[2]} className="form-control" placeholder="Enter Amount" />
                                             <label className="form-label responsive-label">Lump Sum Pension (Pre-tax)</label>
-                                            <input type="number" onChange={handleAssetYouChange(3)} className="form-control" placeholder="Enter Amount" />
+                                            <input type="number" onChange={handleAssetYouChange(3)} value={youAssetArray[3] === 0 ? "" : youAssetArray[3]} className="form-control" placeholder="Enter Amount" />
                                             <label className="form-label responsive-label">Long-Term Care Insurance</label>
-                                            <input type="number" onChange={handleAssetYouChange(4)} className="form-control" placeholder="Enter Amount" />
+                                            <input type="number" onChange={handleAssetYouChange(4)} value={youAssetArray[4] === 0 ? "" : youAssetArray[4]} className="form-control" placeholder="Enter Amount" />
                                             <label className="form-label responsive-label">Life Insurance</label>
-                                            <input type="number" onChange={handleAssetYouChange(5)} className="form-control" placeholder="Enter Amount" />
+                                            <input type="number" onChange={handleAssetYouChange(5)} value={youAssetArray[5] === 0 ? "" : youAssetArray[5]} className="form-control" placeholder="Enter Amount" />
                                             <label className="form-label responsive-label">Business Interest</label>
-                                            <input type="number" onChange={handleAssetYouChange(6)} className="form-control" placeholder="Enter Amount" />
+                                            <input type="number" onChange={handleAssetYouChange(6)} value={youAssetArray[6] === 0 ? "" : youAssetArray[6]} className="form-control" placeholder="Enter Amount" />
                                             <label className="form-label responsive-label">Other Assets</label>
                                             <input type="number" onChange={handleAssetYouChange(7)} value={youAssetArray[7] === 0 ? "" : youAssetArray[7]} className="form-control" placeholder="Enter Amount" />
                                             <label className="form-label responsive-label">Total</label>
@@ -188,19 +188,19 @@ function Step2FinancialStrategy() {
                                         <div className="personal-detail-input">
                                             <h2>Spouse</h2>
                                             <label className="form-label responsive-label">Home Value</label>
-                                            <input type="number" onChange={handleAssetSpouseChange(0)} className="form-control" placeholder="Enter Amount" />
+                                            <input type="number" onChange={handleAssetSpouseChange(0)} value={spouseAssetArray[0] === 0 ? "" : spouseAssetArray[0]} className="form-control" placeholder="Enter Amount" />
                                             <label className="form-label responsive-label">Vacation Homes</label>
-                                            <input type="number" onChange={handleAssetSpouseChange(1)} className="form-control" placeholder="Enter Amount" />
+                                            <input type="number" onChange={handleAssetSpouseChange(1)} value={spouseAssetArray[1] === 0 ? "" : spouseAssetArray[1]} className="form-control" placeholder="Enter Amount" />
                                             <label className="form-label responsive-label">Rental Properties</label>
-                                            <input type="number" onChange={handleAssetSpouseChange(2)} className="form-control" placeholder="Enter Amount" />
+                                            <input type="number" onChange={handleAssetSpouseChange(2)} value={spouseAssetArray[2] === 0 ? "" : spouseAssetArray[2]} className="form-control" placeholder="Enter Amount" />
                                             <label className="form-label responsive-label">Lump Sum Pension (Pre-tax)</label>
-                                            <input type="number" onChange={handleAssetSpouseChange(3)} className="form-control" placeholder="Enter Amount" />
+                                            <input type="number" onChange={handleAssetSpouseChange(3)} value={spouseAssetArray[3] === 0 ? "" : spouseAssetArray[3]} className="form-control" placeholder="Enter Amount" />
                                             <label className="form-label responsive-label">Long-Term Care Insurance</label>
-                                            <input type="number" onChange={handleAssetSpouseChange(4)} className="form-control" placeholder="Enter Amount" />
+                                            <input type="number" onChange={handleAssetSpouseChange(4)} value={spouseAssetArray[4] === 0 ? "" : spouseAssetArray[4]} className="form-control" placeholder="Enter Amount" />
                                             <label className="form-label responsive-label">Life Insurance</label>
-                                            <input type="number" onChange={handleAssetSpouseChange(5)} className="form-control" placeholder="Enter Amount" />
+                                            <input type="number" onChange={handleAssetSpouseChange(5)} value={spouseAssetArray[5] === 0 ? "" : spouseAssetArray[5]} className="form-control" placeholder="Enter Amount" />
                                             <label className="form-label responsive-label">Business Interest</label>
-                                            <input type="number" onChange={handleAssetSpouseChange(6)} className="form-control" placeholder="Enter Amount" />
+                                            <input type="number" onChange={handleAssetSpouseChange(6)} value={spouseAssetArray[6] === 0 ? "" : spouseAssetArray[6]} className="form-control" placeholder="Enter Amount" />
                                             <label className="form-label responsive-label">Other Assets</label>
                                             <input type="number" onChange={handleAssetSpouseChange(7)} value={spouseAssetArray[7] === 0 ? "" : spouseAssetArray[7]} className="form-control" placeholder="Enter Amount" />
                                             <label className="form-label responsive-label">Total</label>
