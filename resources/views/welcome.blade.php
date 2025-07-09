a55bc962d0a0ea4bc097738343c929a8cb1d7f49
UsmanGhani552 <usman.centosquare@gmail.com>
2025-07-09 13:55:14 +0500

    app name changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Peak America</title>
+    <title>{{ env('APP_NAME') }}</title>
     <link rel="icon" type="image/png" href="{{ asset('public/logo.png') }}">
     <!-- remix icon font css  -->
     <link rel="stylesheet" href="{{ asset('assets/css/remixicon.css') }}">
